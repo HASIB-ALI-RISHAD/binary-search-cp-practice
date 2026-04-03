bac5fc834b4945137a1c1045968164dc4cdbf734
HASIB-ALI-RISHAD <hasib.ali.rishad007@gmail.com>
2026-04-03 23:54:11 +0600

    Add solution for "K-th Not Divisible by n" problem

---
+++ K_th_Not_Divisible_by_n.py
@@ -0,0 +1,69 @@
+import sys, os, bisect, heapq, math
+from collections import deque, defaultdict, Counter
+from itertools import permutations, combinations
+sys.setrecursionlimit(10**6)
+sys.set_int_max_str_digits(0) 
+inf = 10**18
+mod = 1000000007 
+
+if os.path.exists('input.txt'):
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+
+
+def get_int(): 
+    return int(sys.stdin.readline())
+
+def get_ints(): 
+    return map(int, sys.stdin.readline().split())
+
+def get_list(): 
+    return list(map(int, sys.stdin.readline().split()))
+
+def get_string(): 
+    return sys.stdin.readline().rstrip()
+
+def get_words(): 
+    return sys.stdin.readline().split()
+
+def fast_print(ans):
+    sys.stdout.write(str(ans) + '\n')
+
+def print_list(arr):
+    sys.stdout.write(" ".join(map(str, arr)) + '\n')
+
+
+
+
+
+def solve():
+    n , k = get_ints()
+    l = 1
+    r = 2*(10**9)
+    ans = -1
+
+    def predicate(mid):
+       return mid - (mid//n) >= k
+
+
+    while l<=r:
+        mid = l + (r-l)//2
+
+        if predicate(mid):
+            ans = mid
+            r = mid - 1
+        else:
+            l = mid + 1
+    fast_print(ans)
+
+
+    
+
+if __name__ == '__main__':
+    # multiple test cases
+    t = get_int()
+    for _ in range(t):
+        solve()
+    
+    # single test case
+    # solve()
